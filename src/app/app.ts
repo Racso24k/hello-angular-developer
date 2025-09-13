ef98fbed822ace69b59c4d0fa19472e2517e28be
Racso24k <u20211g192@upc.edu.pe>
2025-09-13 15:53:37 -0500

    chore:  default setup

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,10 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('hello-angular-developer');
+  protected readonly title = signal('hello-Renatiño');
 }
